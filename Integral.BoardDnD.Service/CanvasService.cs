78fa23ce42223d1ddccb4cba30cedada7dde96ff
Fedor Hajdu <fedorhajdu@gmail.com>
2013-08-14 09:21:53 +0200

    Include json.net and use it to deserialize case sensitive json

--- Integral.BoardDnD.Service/CanvasService.cs
+++ Integral.BoardDnD.Service/CanvasService.cs
@@ -8,13 +8,14 @@ namespace Integral.BoardDnD.Service
     {
         public static Canvas GetCanvasEntity(int campaignId)
         {
-            //var text = File.ReadAllText(@"TestData\TestJSON.txt");
-            //var c = Serializer.Deserialize<Entity.Canvas>(text);
+            var text = File.ReadAllText(@"TestData\TestJSON.txt");
+            var c = Serializer.Deserialize<Entity.Canvas>(text);
 
-            var text = File.ReadAllText(@"TestData\SingleEntity.txt");
-            var c = Serializer.Deserialize<Entity.Base2>(text);
+            //var text = File.ReadAllText(@"TestData\SingleEntity.txt");
+            //var c = Serializer.Deserialize<Entity.Base>(text);
 
-            return null;
+
+            return c;
         }
 
         public static void UpdateCanvas(int campaignId, Canvas canvas)
--- Integral.BoardDnD.Service/Serializer.cs
+++ Integral.BoardDnD.Service/Serializer.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.Serialization.Json;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace Integral.BoardDnD.Service
 {
@@ -12,21 +13,28 @@ namespace Integral.BoardDnD.Service
     {
         public static T Deserialize<T>(string json)
         {
-            var obj = Activator.CreateInstance<T>();
-            var ms = new MemoryStream(Encoding.Unicode.GetBytes(json));
-            var serializer = new DataContractJsonSerializer(obj.GetType());
-            obj = (T)serializer.ReadObject(ms);
-            ms.Close();
-            return obj;
+            return JsonConvert.DeserializeObject<T>(json);
+            //Newtonsoft.Json.JsonSerializer.Create()
         }
 
-        public static string Serialize<T>(T obj)
-        {
-            var serializer = new DataContractJsonSerializer(obj.GetType());
-            var ms = new MemoryStream();
-            serializer.WriteObject(ms, obj);
-            string retVal = Encoding.UTF8.GetString(ms.ToArray());
-            return retVal;
-        }
+
+        //public static T Deserialize<T>(string json)
+        //{
+        //    var obj = Activator.CreateInstance<T>();
+        //    var ms = new MemoryStream(Encoding.Unicode.GetBytes(json));
+        //    var serializer = new DataContractJsonSerializer(obj.GetType());
+        //    obj = (T)serializer.ReadObject(ms);
+        //    ms.Close();
+        //    return obj;
+        //}
+
+        //public static string Serialize<T>(T obj)
+        //{
+        //    var serializer = new DataContractJsonSerializer(obj.GetType());
+        //    var ms = new MemoryStream();
+        //    serializer.WriteObject(ms, obj);
+        //    string retVal = Encoding.UTF8.GetString(ms.ToArray());
+        //    return retVal;
+        //}
     }
 }
