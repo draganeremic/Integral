349a9c48c6aaa6ac7b8337ea229fe4ab8641335f
draganeremic <draganeremic@gmail.com>
2013-10-01 20:36:17 +0200

    Added Background.js.

---
+++ Integral.BoardDnD.Web/Scripts/app/models/Background.js
@@ -0,0 +1,19 @@
+﻿var Background = fabric.util.createClass(BaseModel, {
+    type: 'Background',
+
+    initialize: function (element, options) {
+        options || (options = {});
+
+        this.callSuper('initialize', element, options);
+    },
+    toObject: function () {
+        return fabric.util.object.extend(this.callSuper('toObject'), {
+        });
+    }
+});
+
+Background.FromURL = function (url, callback, imgOptions) {
+    fabric.util.loadImage(url, function (img) {
+        callback(new Background(img, imgOptions));
+    });
+};
